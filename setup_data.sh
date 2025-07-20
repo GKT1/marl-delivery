1d4bcd2d9e9669f7fbb92d6fecee24c3f6331de8
GKT1 <41571694+GKT1@users.noreply.github.com>
2025-07-21 05:25:02 +0700

    Update setup_data.sh

--- setup_data.sh
+++ setup_data.sh
@@ -46,20 +46,20 @@ LORA_MODELS=(
     "https://huggingface.co/GKT/lora1_rank32_fp16-step01000/resolve/main/lora1_rank32_fp16-step01000.safetensors"
 )
 VAE_MODELS=(
-    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
+    # "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
 )
 ESRGAN_MODELS=( )
 CONTROLNET_MODELS=(
     "https://civitai.com/api/download/models/1480637?type=Model&format=SafeTensor&token=c31e72d268968c10449529820d8b417f"
 )
 CLIP_VISION_MODELS=(
-    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors"
+    # "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/clip_vision/clip_vision_h.safetensors"
 )
 DIFFUSION_MODELS=(
-    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_480p_14B_fp8_e4m3fn.safetensors"
+    # "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/diffusion_models/wan2.1_i2v_480p_14B_fp8_e4m3fn.safetensors"
 )
 TEXT_ENCODERS_MODELS=(
-    "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
+    # "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors"
 )
 
 
